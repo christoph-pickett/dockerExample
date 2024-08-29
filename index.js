3e0b9f4f0524ee50eadce1638553a99b4c5a2315
Christoph <christoph-85@hotmail.com>
2024-08-29 19:40:39 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+var cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World asdasdasdada!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
